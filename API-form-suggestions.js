b9a91775239e9ab4e5d8e065ce677aa06417fd3a
abcristian <bogdan.cristian.anton@gmail.com>
2018-03-22 01:05:50 +0000

    requirements and design sections added

--- API-form-suggestions.js
+++ API-form-suggestions.js
@@ -3,6 +3,8 @@ function myFunction(){
 	var firstName = $("#firstname").val();
 	var lastName = $("#lastname").val();
 
+	const users = [{"firstName":"ttt","lastName":"test"},{"firstName":"abc","lastName":"test"},{"firstName":"def","lastName":"test"},{"firstName":"Rick","lastName":"Clarke"},{"firstName":"Rick","lastName":"Clarke"},{"firstName":"Richard","lastName":"Clarke"},{"firstName":"Richard","lastName":"Clarke"},{"firstName":"Richard","lastName":"Clarke"},{"firstName":"Richard","lastName":"Clarke"},{"firstName":"dc","lastName":"d"},{"firstName":"dsads","lastName":"sdada"}]
+
 	var data =
 	  {
 		  "firstName": firstName,
@@ -19,18 +21,22 @@ function myFunction(){
 	
 
 
-
-	$.ajax({
-	  type: "POST",
-	  url: 'http://citiesunlockedapitest.azurewebsites.net/users/inf/3',
-	  data: JSON.stringify(data),
-	  dataType: "json",
-	  beforeSend: function(xhr){xhr.setRequestHeader('Content-Type', 'application/json');},
-	  success: function(response) {
-	  	console.log(response);
-	  	//window.location = "file:///Users/sonik/Desktop/Cities%20Unlocked%20Presentation%20Website/API-form-suggestions.html";
-	  }
-	});
+	for(const user in users) {
+		data["firstName"] = user.firstName
+		data["lastName"] = user.lastName
+		$.ajax({
+		  type: "POST",
+		  url: 'http://citiesunlockedapitest.azurewebsites.net/users/inf/3',
+		  data: JSON.stringify(data),
+		  dataType: "json",
+		  beforeSend: function(xhr){xhr.setRequestHeader('Content-Type', 'application/json');},
+		  success: function(response) {
+		  	console.log(response.length ? response : user);
+		  	//window.location = "file:///Users/sonik/Desktop/Cities%20Unlocked%20Presentation%20Website/API-form-suggestions.html";
+		  }
+		});
+	}
+	
 
 }
 
